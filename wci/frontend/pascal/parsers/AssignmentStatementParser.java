3b7b21d64987d7eb5a4d138c8560fa0f57911f45
Stidham <astidham2003@gmail.com>
2017-09-12 20:36:27 -0400

    Completed AssignmentParser first pass.

--- wci/frontend/pascal/parsers/AssignmentStatementParser.java
+++ wci/frontend/pascal/parsers/AssignmentStatementParser.java
@@ -1,9 +1,19 @@
 package wci.frontend.pascal.parsers;
 
 import wci.intermediate.ICodeNode;
+import wci.intermediate.ICodeFactory;
+import wci.intermediate.SymTabEntry;
 
 import wci.frontend.Token;
 import wci.frontend.pascal.PascalParserTD;
+
+import static wci.intermediate.icodeimpl.ICodeKeyImpl.ID;
+
+import static wci.intermediate.icodeimpl.ICodeNodeTypeImpl.ASSIGN;
+import static wci.intermediate.icodeimpl.ICodeNodeTypeImpl.VARIABLE;
+
+import static wci.frontend.pascal.PascalTokenType.COLON_EQUALS;
+import static wci.frontend.pascal.PascalErrorCode.MISSING_COLON_EQUALS;
 
 /**
 * <h1>AssignmentStatementParser</h1>
@@ -32,8 +42,44 @@ public class AssignmentStatementParser
 	*/
 	public ICodeNode parse(Token token) throws Exception
 	{
-		ICodeNode assignmentStatement = null;
-		return assignmentStatement;
+		ICodeNode assignNode = ICodeFactory.createICodeNode(ASSIGN);
+
+		// Look up the target identifier in the symbol table stack.
+		// Enter the identifier into the table if it's not found.
+		String targetName = token.getText().toLowerCase();
+		SymTabEntry targetId = symTabStack.lookup(targetName);
+		if (targetId == null) {
+			// XXX For now, consider a variable declared the first
+			// XXX time it appears.
+			targetId = symTabStack.enterLocal(targetName);
+		}
+		targetId.appendLineNumber(token.getLineNumber());
+
+		token = nextToken(); // Consume the identifier token.
+
+		// Create the variable node and set its name attribute.
+		ICodeNode variableNode =
+			ICodeFactory.createICodeNode(VARIABLE);
+		// XXX For now, every variable is an identifier.
+		variableNode.setAttribute(ID,targetId);
+
+		// The ASSIGN node adopts the variable node as its first child.
+		assignNode.addChild(variableNode);
+
+		// Look for the := token.
+		if (token.getType() == COLON_EQUALS) {
+			nextToken(); // Consume the :=.
+		}
+		else {
+			errorHandler.flag(token,MISSING_COLON_EQUALS,this);
+		}
+
+		// Parse the expression.  The ASSIGN node adopts the
+		// expression's node as its second child.
+		ExpressionParser expressionParser = new ExpressionParser(this);
+		assignNode.addChild(expressionParser.parse(token));
+
+		return assignNode;
 	}
 }
 
---
+++ wci/frontend/pascal/parsers/ExpressionParser.java
@@ -0,0 +1,15 @@
+package wci.frontend.pascal.parsers;
+
+import wci.frontend.pascal.PascalParserTD;
+
+/**
+* <h1>ExpressionParser</h1>
+*/
+public class ExpressionParser extends StatementParser
+{
+	public ExpressionParser(PascalParserTD parent)
+	{
+		super(parent);
+	}
+}
+
