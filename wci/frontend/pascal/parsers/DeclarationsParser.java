a4d5cd7c6aabc4234a1590940fc00f2338c55e6a
Stidham <astidham2003@gmail.com>
2017-10-24 22:27:49 -0400

    Updated Declarations parser.  Leaving off with compilition errors.  Need to create the other *DefinitionsParsers and the VariableDeclarationsParser.

--- wci/frontend/pascal/parsers/DeclarationsParser.java
+++ wci/frontend/pascal/parsers/DeclarationsParser.java
@@ -11,6 +11,8 @@ import wci.intermediate.ICodeNode;
 
 import static wci.frontend.pascal.PascalTokenType.*;
 
+import static wci.intermediate.symtabimpl.DefinitionImpl.VARIABLE;
+
 /**
 * <h1>DeclarationsParser</h1>
 *
@@ -28,9 +30,13 @@ public class DeclarationsParser extends PascalParserTD
 	static final EnumSet<PascalTokenType> VAR_START_SET =
 		TYPE_START_SET.clone();
 
+	static final EnumSet<PascalTokenType> ROUTINE_START_SET =
+		VAR_START_SET.clone();
+
 	static {
 		TYPE_START_SET.remove(CONST);
 		VAR_START_SET.remove(TYPE);
+		ROUTINE_START_SET.remove(VAR);
 	}
 
 	/**
@@ -47,13 +53,46 @@ public class DeclarationsParser extends PascalParserTD
 
 	/**
 	* Parse declarations.
+	* To be overridden by the specialized declarations parser
+	* subclasses.
 	*
 	* @param token the initial token
 	* @return the root node of the parse tree
 	*/
-	public ICodeNode parse(Token token)
+	public void parse(Token token) throws Exception
 	{
-		return null;
+		token = synchronize(DECLARATION_START_SET);
+
+		if (token.getType() == CONST) {
+			token = nextToken(); // Consume the CONST token.
+
+			ConstantDefinitionsParser constantDefinitionsParser =
+				new ConstantDefinitionsParser(this);
+			constantDefinitionsParser.parse(token);
+		}
+
+		token = synchronize(TYPE_START_SET);
+
+		if (token.getType() == TYPE) {
+			token = nextToken();  // Consume the TYPE token.
+
+			TypeDefinitionsParser typeDefinitionsParser =
+				new TypeDefinitionsParser(this);
+			typeDefinitionsParser.parse(token);
+		}
+
+		token = synchronize(VAR_START_SET);
+
+		if (token.getType() == VAR) {
+			token = nextToken(); // Consume the VAR token.
+
+			VariableDeclarationsParser variableDeclarationsParser =
+				new VariableDeclarationsParser(this);
+			variableDeclarationsParser.setDefinition(VARIABLE);
+			variableDeclarationsParser.parse(token);
+		}
+
+		token = synchronize(ROUTINE_START_SET);
 	}
 }
 
