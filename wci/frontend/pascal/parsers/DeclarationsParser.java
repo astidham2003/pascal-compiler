0dcc116b8c28d4d88d6a7d5e2e6bd016a0e9f9a0
Stidham <astidham2003@gmail.com>
2017-10-24 21:44:03 -0400

    Fixed compile errors.

--- wci/frontend/pascal/parsers/DeclarationsParser.java
+++ wci/frontend/pascal/parsers/DeclarationsParser.java
@@ -1,10 +1,15 @@
 package wci.frontend.pascal.parsers;
 
+import java.util.EnumSet;
+
 import wci.frontend.Token;
 
 import wci.frontend.pascal.PascalParserTD;
+import wci.frontend.pascal.PascalTokenType;
 
 import wci.intermediate.ICodeNode;
+
+import static wci.frontend.pascal.PascalTokenType.*;
 
 /**
 * <h1>DeclarationsParser</h1>
@@ -13,6 +18,20 @@ import wci.intermediate.ICodeNode;
 */
 public class DeclarationsParser extends PascalParserTD
 {
+
+	static final EnumSet<PascalTokenType> DECLARATION_START_SET =
+		EnumSet.of(CONST,TYPE,VAR,PROCEDURE,FUNCTION,BEGIN);
+
+	static final EnumSet<PascalTokenType> TYPE_START_SET =
+		DECLARATION_START_SET.clone();
+
+	static final EnumSet<PascalTokenType> VAR_START_SET =
+		TYPE_START_SET.clone();
+
+	static {
+		TYPE_START_SET.remove(CONST);
+		VAR_START_SET.remove(TYPE);
+	}
 
 	/**
 	* Constructor
